fdc6aaa1deb7b882d5e58836d90e7f725146db8d
Yegua Celestial <yeguacelestial@gmail.com>
2020-11-20 14:07:02 -0600

    Chapter 4 - Connected Django API

--- todo/frontend/src/App.js
+++ todo/frontend/src/App.js
@@ -1,39 +1,36 @@
 import React, { Component } from 'react'
+import axios from 'axios' // new
 
-import logo from './logo.svg';
-import './App.css';
-
-const list = [
-  {
-    "id": 1,
-    "title": "1st todo",
-    "body": "Learn Django properly"
-  },
-  {
-    "id": 2,
-    "title": "Second item",
-    "body": "Learn Python"
-  },
-  {
-    "id": 3,
-    "title": "Learn HTTP",
-    "body": "It's important"
-  },
-]
 
 class App extends Component {
-  constructor(props) {
-    super(props)
-    this.state = { list }
+  state = {
+    todos: []
+  }
+
+  // new
+  componentDidMount() {
+    this.getTodos()
+  }
+
+  // new
+  getTodos() {
+    axios
+      .get('http://127.0.0.1:8000/api/')
+      .then(res => {
+        this.setState({ todos: res.data })
+      })
+      .catch(err => {
+        console.log(err)
+      })
   }
 
   render() {
     return (
       <div>
-        {this.state.list.map(item => (
+        {this.state.todos.map(item => (
           <div key={item.id}>
             <h1>{item.title}</h1>
-            <p>{item.body}</p>
+            <span>{item.body}</span>
           </div>
         ))}
       </div>
