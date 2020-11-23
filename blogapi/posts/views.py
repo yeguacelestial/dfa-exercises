f4eab2020b072b2f91fc9867a89a7fe8fe29bed6
Yegua Celestial <yeguacelestial@gmail.com>
2020-11-23 14:34:14 -0600

    Chapter 6 - Modify BasePermission methods

--- blogapi/posts/views.py
+++ blogapi/posts/views.py
@@ -1,7 +1,10 @@
 from django.shortcuts import render
 
 from rest_framework import generics, permissions  # new
+from rest_framework import generics
+
 from .models import Post
+from .permissions import IsAuthorOrReadOnly  # new
 from .serializers import PostSerializer
 
 
@@ -13,5 +16,6 @@ class PostList(generics.ListCreateAPIView):
 
 # Retrieving, updating and destroying a post
 class PostDetail(generics.RetrieveUpdateDestroyAPIView):
+    permission_classes = (IsAuthorOrReadOnly, )  # new
     queryset = Post.objects.all()
     serializer_class = PostSerializer
