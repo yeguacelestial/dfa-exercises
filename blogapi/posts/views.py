b0e2bb4fdbe318034cba04bfc3f9a9cf8574867f
Yegua Celestial <yeguacelestial@gmail.com>
2020-11-23 14:12:04 -0600

    Chapter 6 - Add authentication permissions to API (View-level)

--- blogapi/posts/views.py
+++ blogapi/posts/views.py
@@ -1,17 +1,19 @@
 from django.shortcuts import render
 
-from rest_framework import generics
+from rest_framework import generics, permissions  # new
 from .models import Post
 from .serializers import PostSerializer
 
 
 # Get all the posts
 class PostList(generics.ListCreateAPIView):
+    permission_classes = (permissions.IsAuthenticated, )
     queryset = Post.objects.all()
     serializer_class = PostSerializer
 
 
 # Retrieving, updating and destroying a post
 class PostDetail(generics.RetrieveUpdateDestroyAPIView):
+    permission_classes = (permissions.IsAuthenticated, )
     queryset = Post.objects.all()
     serializer_class = PostSerializer
