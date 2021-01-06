19532146ca89b27dcc08833aa23b8407845f3699
yeguacelestial <yeguacelestial@gmail.com>
2021-01-05 22:48:13 -0600

    Chapter 8 - Create router for endpoints

--- blogapi/posts/urls.py
+++ blogapi/posts/urls.py
@@ -1,9 +1,11 @@
 from django.urls import path
-from .views import UserList, UserDetail, PostList, PostDetail
+from rest_framework.routers import SimpleRouter
 
-urlpatterns = [
-    path('users/', UserList.as_view()),
-    path('users/<int:pk>', UserDetail.as_view()),
-    path('', PostList.as_view()),
-    path('<int:pk>/', PostDetail.as_view()),
-]
+from .views import UserViewSet, PostViewSet
+
+
+router = SimpleRouter()
+router.register('users', UserViewSet, basename='users')
+router.register('', PostViewSet, basename='posts')
+
+urlpatterns = router.urls
