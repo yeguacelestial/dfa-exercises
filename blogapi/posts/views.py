3c4f46073515e90e1f5a83d01414b1e45cfd4e4d
yeguacelestial <yeguacelestial@gmail.com>
2021-01-05 22:42:00 -0600

    Chapter 8 - Change List and Detail views to viewsets

--- blogapi/posts/views.py
+++ blogapi/posts/views.py
@@ -1,32 +1,19 @@
 from django.shortcuts import render
 from django.contrib.auth import get_user_model
 
-from rest_framework import generics, permissions
-from rest_framework import generics
+from rest_framework import viewsets
 
 from .models import Post
 from .permissions import IsAuthorOrReadOnly
 from .serializers import PostSerializer, UserSerializer
 
 
-# Get all the posts
-class PostList(generics.ListCreateAPIView):
-    queryset = Post.objects.all()
-    serializer_class = PostSerializer
-
-
-# Retrieving, updating and destroying a post
-class PostDetail(generics.RetrieveUpdateDestroyAPIView):
+class PostViewSet(viewsets.ModelViewSet):
     permission_classes = (IsAuthorOrReadOnly, )
     queryset = Post.objects.all()
     serializer_class = PostSerializer
 
 
-class UserList(generics.ListCreateAPIView):
-    queryset = get_user_model().objects.all()
-    serializer_class = UserSerializer
-
-
-class UserDetail(generics.RetrieveUpdateDestroyAPIView):
+class UserViewSet(viewsets.ModelViewSet):
     queryset = get_user_model().objects.all()
     serializer_class = UserSerializer
