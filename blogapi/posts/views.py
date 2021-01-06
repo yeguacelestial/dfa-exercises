6de995252e4eb84af919db46df1eb9d2905ab0b2
yeguacelestial <yeguacelestial@gmail.com>
2021-01-05 22:34:54 -0600

    Chapter 8 - Create endpoints for UserList and UserDetail

--- blogapi/posts/views.py
+++ blogapi/posts/views.py
@@ -1,11 +1,12 @@
 from django.shortcuts import render
+from django.contrib.auth import get_user_model
 
-from rest_framework import generics, permissions  # new
+from rest_framework import generics, permissions
 from rest_framework import generics
 
 from .models import Post
-from .permissions import IsAuthorOrReadOnly  # new
-from .serializers import PostSerializer
+from .permissions import IsAuthorOrReadOnly
+from .serializers import PostSerializer, UserSerializer
 
 
 # Get all the posts
@@ -16,6 +17,16 @@ class PostList(generics.ListCreateAPIView):
 
 # Retrieving, updating and destroying a post
 class PostDetail(generics.RetrieveUpdateDestroyAPIView):
-    permission_classes = (IsAuthorOrReadOnly, )  # new
+    permission_classes = (IsAuthorOrReadOnly, )
     queryset = Post.objects.all()
     serializer_class = PostSerializer
+
+
+class UserList(generics.ListCreateAPIView):
+    queryset = get_user_model().objects.all()
+    serializer_class = UserSerializer
+
+
+class UserDetail(generics.RetrieveUpdateDestroyAPIView):
+    queryset = get_user_model().objects.all()
+    serializer_class = UserSerializer
